01ec4c04bc9f0f83e413fc9bd6ad1ae1b1369de1
thomas-vaiarella <vaiarella.t@husky.neu.edu>
2020-01-28 11:19:35 -0500

    Moved main test function outside class

--- test-map.cpp
+++ test-map.cpp
@@ -4,133 +4,130 @@
 #include "map.h"
 #include "string.h"
 
-class TestMap {
-  public:
-    bool it_returns_the_value_for_a_key() {
-      Map* map = new Map();
-      String* foo_key = new String("fooKey");
-      String* foo = new String("foo");
-      map->put(foo_key, foo);
-      return map->get(foo_key)->equals(foo);
+bool it_returns_the_value_for_a_key() {
+  Map* map = new Map();
+  String* foo_key = new String("fooKey");
+  String* foo = new String("foo");
+  map->put(foo_key, foo);
+  return map->get(foo_key)->equals(foo);
+}
+
+bool it_contains_the_key() {
+  Map* map = new Map();
+  String* foo_key = new String("fooKey");
+  String* foo = new String("foo");
+  map->put(foo_key, foo);
+  return map->contains_key(foo_key) && !map->contains_key(foo);
+}
+
+bool it_equals_another_empty_map() {
+  Map* map1 = new Map();
+  Map* map2 = new Map();
+  return map1->equals(map2);
+}
+
+bool it_equals_another_map() {
+  Map* map1 = new Map();
+  Map* map2 = new Map();
+  String* foo_key = new String("fooKey");
+  String* foo = new String("foo");
+  map1->put(foo_key, foo);
+  map2->put(foo_key, foo);
+  return map1->equals(map2);
+}
+
+bool it_computes_the_same_empty_hashcode() {
+  Map* map1 = new Map();
+  Map* map2 = new Map();
+  return map1->hash() == map2->hash();
+}
+
+bool it_computes_the_same_hashcode() {
+  Map* map1 = new Map();
+  Map* map2 = new Map();
+  String* foo_key = new String("fooKey");
+  String* foo = new String("foo");
+  map1->put(foo_key, foo);
+  map2->put(foo_key, foo);
+  return map1->hash() == map2->hash();
+}
+
+bool it_computes_keyset() {
+  Map* map = new Map();
+  String* foo_key = new String("fooKey");
+  String* bar_key = new String("barKey");
+  String* foo = new String("foo");
+  String* bar = new String("bar");
+  map->put(foo_key, foo);
+  map->put(bar_key, bar);
+  Object** keys = map->key_set();
+
+  bool containsFoo = false;
+  bool containsBar = false;
+  for (int i = 0; i < map->size(); i++) {
+    if (keys[i]->equals(foo_key)) {
+      containsFoo = true;
     }
-
-    bool it_contains_the_key() {
-      Map* map = new Map();
-      String* foo_key = new String("fooKey");
-      String* foo = new String("foo");
-      map->put(foo_key, foo);
-      return map->contains_key(foo_key) && !map->contains_key(foo);
+    if (keys[i]->equals(bar_key)) {
+      containsBar = true;
     }
+  }
 
-    bool it_equals_another_empty_map() {
-      Map* map1 = new Map();
-      Map* map2 = new Map();
-      return map1->equals(map2);
-    }
+  return containsFoo && containsBar && map->size() == 2;
+}
 
-    bool it_equals_another_map() {
-      Map* map1 = new Map();
-      Map* map2 = new Map();
-      String* foo_key = new String("fooKey");
-      String* foo = new String("foo");
-      map1->put(foo_key, foo);
-      map2->put(foo_key, foo);
-      return map1->equals(map2);
-    }
+bool it_removes_elements() {
+  Map* map = new Map();
+  String* foo_key = new String("fooKey");
+  String* bar_key = new String("barKey");
+  String* foo = new String("foo");
+  String* bar = new String("bar");
+  map->put(foo_key, foo);
+  map->put(bar_key, bar);
 
-    bool it_computes_the_same_empty_hashcode() {
-      Map* map1 = new Map();
-      Map* map2 = new Map();
-      return map1->hash() == map2->hash();
-    }
+  if (map->size() != 2) return false;
 
-    bool it_computes_the_same_hashcode() {
-      Map* map1 = new Map();
-      Map* map2 = new Map();
-      String* foo_key = new String("fooKey");
-      String* foo = new String("foo");
-      map1->put(foo_key, foo);
-      map2->put(foo_key, foo);
-      return map1->hash() == map2->hash();
-    }
+  Object* foo_removed = map->remove(foo_key);
+  if (map->size() != 1) return false;
+  if (!foo->equals(foo_removed)) return false;
 
-    bool it_computes_keyset() {
-      Map* map = new Map();
-      String* foo_key = new String("fooKey");
-      String* bar_key = new String("barKey");
-      String* foo = new String("foo");
-      String* bar = new String("bar");
-      map->put(foo_key, foo);
-      map->put(bar_key, bar);
-      Object** keys = map->key_set();
+  Object* bar_removed = map->remove(bar_key);
+  if (map->size() != 0) return false;
+  return bar->equals(bar_removed);
+}
 
-      bool containsFoo = false;
-      bool containsBar = false;
-      for (int i = 0; i < map->size(); i++) {
-        if (keys[i]->equals(foo_key)) {
-          containsFoo = true;
-        }
-        if (keys[i]->equals(bar_key)) {
-          containsBar = true;
-        }
-      }
+bool it_computes_size() {
+  Map* map = new Map();
+  if (map->size() != 0) return false;
+  String* foo_key = new String("fooKey");
+  String* foo = new String("foo");
+  map->put(foo_key, foo);
+  return map->size() == 1;
+}
 
-      return containsFoo && containsBar && map->size() == 2;
-    }
+bool it_updates_values() {
+  Map* map = new Map();
+  String* foo_key = new String("fooKey");
+  String* foo = new String("foo");
+  String* bar = new String("bar");
 
-    bool it_removes_elements() {
-      Map* map = new Map();
-      String* foo_key = new String("fooKey");
-      String* bar_key = new String("barKey");
-      String* foo = new String("foo");
-      String* bar = new String("bar");
-      map->put(foo_key, foo);
-      map->put(bar_key, bar);
+  map->put(foo_key, foo);
+  if (!map->get(foo_key)->equals(foo)) return false;
 
-      if (map->size() != 2) return false;
+  map->put(foo_key, bar);
+  return map->get(foo_key)->equals(bar);
+}
 
-      Object* foo_removed = map->remove(foo_key);
-      if (map->size() != 1) return false;
-      if (!foo->equals(foo_removed)) return false;
-
-      Object* bar_removed = map->remove(bar_key);
-      if (map->size() != 0) return false;
-      return bar->equals(bar_removed);
-    }
-
-    bool it_computes_size() {
-      Map* map = new Map();
-      if (map->size() != 0) return false;
-      String* foo_key = new String("fooKey");
-      String* foo = new String("foo");
-      map->put(foo_key, foo);
-      return map->size() == 1;
-    }
-
-    bool it_updates_values() {
-      Map* map = new Map();
-      String* foo_key = new String("fooKey");
-      String* foo = new String("foo");
-      String* bar = new String("bar");
-
-      map->put(foo_key, foo);
-      if (!map->get(foo_key)->equals(foo)) return false;
-
-      map->put(foo_key, bar);
-      return map->get(foo_key)->equals(bar);
-    }
-
-    int main() {
-      bool success = it_returns_the_value_for_a_key()
-        && it_contains_the_key()
-        && it_equals_another_empty_map()
-        && it_equals_another_map()
-        && it_computes_the_same_empty_hashcode()
-        && it_computes_the_same_hashcode()
-        && it_computes_keyset()
-        && it_removes_elements()
-        && it_computes_size()
-        && it_updates_values();
-      return success ? 0 : 1;
-    }
-};
+int main() {
+  bool success = it_returns_the_value_for_a_key()
+    && it_contains_the_key()
+    && it_equals_another_empty_map()
+    && it_equals_another_map()
+    && it_computes_the_same_empty_hashcode()
+    && it_computes_the_same_hashcode()
+    && it_computes_keyset()
+    && it_removes_elements()
+    && it_computes_size()
+    && it_updates_values();
+  return success ? 0 : 1;
+}
